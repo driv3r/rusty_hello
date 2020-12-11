40297e1b4fdfce58a4eb94dda507065dd2098d4e
Leszek Zalewski <leszekzalewski@fastmail.fm>
2020-12-11 20:11:53 +0100

    Gem init

---
+++ lib/rusty_hello/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RustyHello
+  VERSION = "0.1.0"
+end
---
+++ spec/rusty_hello_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe RustyHello do
+  it "has a version number" do
+    expect(RustyHello::VERSION).not_to be nil
+  end
+end
