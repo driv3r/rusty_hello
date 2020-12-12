34f575a9b926ff1fa3b0e3192ca54913488214b4
Leszek Zalewski <leszekzalewski@fastmail.fm>
2020-12-12 15:18:20 +0100

    Cross compile and test rb files

---
+++ test_ffi.rb
@@ -0,0 +1,4 @@
+require "rubygems"
+require "ffi"
+
+puts "FFI Platform Suffix: #{::FFI::Platform::LIBSUFFIX.inspect}"
---
+++ test_gem.rb
@@ -0,0 +1,4 @@
+require "rubygems"
+require "rusty_hello"
+
+puts "Testing Gem: #{RustyHello[7].inspect}"
